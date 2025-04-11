8e679a8747f9fe1492e3a381728a297bb042a8f5
azcfza2004 <krokodalmistr@gmail.com>
2025-04-11 18:12:24 +0300

    добавление обработки текстовых сообщений из чата и добавление в docx

--- view/controller.py
+++ view/controller.py
@@ -1,17 +1,22 @@
 import asyncio
-
 from aiogram import  types, F, Router
+from aiogram.client.session import aiohttp
 from aiogram.types import CallbackQuery
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
-from . import keyboards as kb
+from docx import Document
+
+from view import keyboards as kb
 
 router = Router()
-
+ChatID = -4612403783
+#создание нового файла (хз как исправить)
+doc = Document()
 class Href(StatesGroup): # состояния
     start = State()
     generate = State()
     callable = State()
+    waiting_for_message = State()
     flag = 0
 
 @router.message(F.text == 'Старт')
@@ -19,11 +24,6 @@ async def echo(message: types.Message,state: FSMContext):
     await message.answer(text = f'Здравствуйте, {message.from_user.first_name}.\nВведите данные')
     await state.set_state(Href.start)
 
-@router.message(F.text) #Обработка любого текста
-async def on_start(message: types.Message, state: FSMContext):
-    await message.answer(text="Нажмите старт",
-                         reply_markup=kb.start)
-    await state.clear()
 
 # Сделать процесс бар
 @router.message(Href.start)
@@ -31,6 +31,21 @@ async def process_bar( message: types.Message, state: FSMContext):
     await state.update_data(href =message.text)
     await message.answer(text='Идёт генерация сводного отчёта')
     await state.set_state(Href.generate)
+
+@router.message(F.text == "Start")
+async def handle_message(message: types.Message, state: FSMContext):
+    await state.set_state(Href.waiting_for_message)
+
+# Сбор информации с чата построчно
+@router.message(Href.waiting_for_message)
+async def handle_message(message: types.Message, state: FSMContext):
+    user_name = message.from_user.full_name
+    id = message.message_id
+    date = message.date
+    text = message.text
+    doc.add_paragraph(f" Имя: {user_name}, Текст: {text}, Дата: {date}, id: {id}")
+    doc.save('history.docx')
+
 
 
 @router.message(Href.generate)
@@ -41,5 +56,4 @@ async def cmd_stop(message: types.Message, state: FSMContext):
     await state.update_data(flag=1)
     await message.answer(text="Что- то пошло так или не так",
                          reply_markup=kb.start)
-
 
