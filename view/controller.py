e0185e3c493117bad419a415c2a0394901b85cc8
azcfza2004 <krokodalmistr@gmail.com>
2025-04-12 12:53:08 +0300

    Добавлено сохранение сообщений в определённый файл

--- view/controller.py
+++ view/controller.py
@@ -5,13 +5,14 @@ from aiogram.types import CallbackQuery
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
 from docx import Document
-
 from view import keyboards as kb
 
 router = Router()
 ChatID = -4612403783
-#создание нового файла (хз как исправить)
-doc = Document()
+
+#Инциализпция работы с файлом history
+doc = Document(r'model\data\history.docx')
+
 class Href(StatesGroup): # состояния
     start = State()
     generate = State()
@@ -21,7 +22,8 @@ class Href(StatesGroup): # состояния
 
 @router.message(F.text == 'Старт')
 async def echo(message: types.Message,state: FSMContext):
-    await message.answer(text = f'Здравствуйте, {message.from_user.first_name}.\nВведите данные')
+    await message.answer(text = f'Здравствуйте, {message.from_user.first_name}.\nВведите данные',
+                         reply_markup=kb.start)
     await state.set_state(Href.start)
 
 
@@ -39,21 +41,10 @@ async def handle_message(message: types.Message, state: FSMContext):
 # Сбор информации с чата построчно
 @router.message(Href.waiting_for_message)
 async def handle_message(message: types.Message, state: FSMContext):
-    user_name = message.from_user.full_name
-    id = message.message_id
+    user_name =  message.from_user.full_name
+    message_id = message.message_id
     date = message.date
     text = message.text
-    doc.add_paragraph(f" Имя: {user_name}, Текст: {text}, Дата: {date}, id: {id}")
-    doc.save('history.docx')
-
-
-
-@router.message(Href.generate)
-async def generate_report (message: types.Message, state:FSMContext):
-    pass
-@router.message(F.text == "Стоп")
-async def cmd_stop(message: types.Message, state: FSMContext):
-    await state.update_data(flag=1)
-    await message.answer(text="Что- то пошло так или не так",
-                         reply_markup=kb.start)
+    doc.add_paragraph(f" Имя: {user_name}, Текст: {text}, Дата: {date}, id сообщения: {message_id}")
+    doc.save(r'model\data\history.docx')
 
