3b7ae8b4fffdff0822a4212780ce0eab19d14e4d
BNLN <shckabat.vlad@yandex.ru>
2025-04-14 03:43:29 +0300

    Добавил чтение данных и формирование jsonl файла для построения таблиц - надо добавить метаданные, но для этого нужна информация как их использовать для построения таблиц

--- model/assistant.py
+++ model/assistant.py
@@ -1,22 +1,17 @@
+from docx import Document
+import json
 import os
+
 import printx
 from yandex_cloud_ml_sdk import YCloudML
 
 from glob import glob
-from tqdm.auto import tqdm
-import pandas as pd
-
-from yandex_cloud_ml_sdk.search_indexes import (
-    StaticIndexChunkingStrategy,
-    HybridSearchIndexType,
-    ReciprocalRankFusionIndexCombinationStrategy,
-)
 
 message_1 = "Проанализируй сообщение и распредели по группам\n Вот пример сообщения: Север \nОтд7 пах с св 41/501\nОтд20 20/281 по пу 61/793\nОтд 3 пах подс.60/231\nПо пу 231\n\nДиск к. Сил отд 7. 32/352\nПу- 484\nДиск под Оз п езубов 20/281\nДиск под с. Св отд 10 83/203 пу-1065га"
-message_2 = "Проанализируй сообщение и распредели по группам\n Вот пример сообщения: Восход\nПосев кук-24/252га\n24%\nПредпосевная культ\nПод кук-94/490га46%\nСЗРоз пш-103/557га\n25%\nПодкормка оз рапс-\n152га , 100%, подкормка овса-97га, 50%\nДовсходовое боронование подсолнечника-524\nга, 100%."
-message_3 = "Проанализируй сообщение и распредели по группам\n Вот пример сообщения: 23.10\nВнесение мин удобрений под оз пшеницу 2025 г ПУ Юг 216/7698\nОтд 17-216/1877"
 
 #Пропускает 1 подкормку
+message_2 = "Проанализируй сообщение и распредели по группам\n Вот пример сообщения: Восход\nПосев кук-24/252га\n24%\nПредпосевная культ\nПод кук-94/490га46%\nСЗРоз пш-103/557га\n 25% \n  Подкормка оз рапс- \n152га , 100%, подкормка овса-97га, 50%\nДовсходовое боронование подсолнечника-524\nга, 100%."
+message_3 = "Проанализируй сообщение и распредели по группам\n Вот пример сообщения: 23.10\nВнесение мин удобрений под оз пшеницу 2025 г ПУ Юг 216/7698\nОтд 17-216/1877"
 message_4 = "Проанализируй сообщение и распредели по группам\n Вот пример сообщения: Пахота зяби под сою \nПо Пу 13/1464\nОтд 17 13/203\n\nДисков сах св\nПо Пу 36/1391\nОтд 16 25/25\nОтд 17 11/664\n\n2-е диск сах св под пш\nПо Пу 111/1288\nОтд 17 111/596\n\nВыравн зяби под сах св\nПо Пу 30/1395\nОтд 11 30/491"
 
 #не распознает выравнивание зяби
@@ -37,14 +32,12 @@ message_7 = "Проанализируй сообщение и распредел
 
 sdk = YCloudML(folder_id="b1g6rjppcrrhq56lsqr0", auth="AQVNxTuU2_Efl4APdsHSsNBwMzOpuLziy4TeHEr2")
 
-#model = sdk.models.completions('yandexgpt')
-###
 #яндекс 5 лайт рс
 #model = (sdk.models.completions("gpt://b1g6rjppcrrhq56lsqr0/yandexgpt-lite/rc@tamrur7jqgegos3foh0dd"))
 
 #LLama
 model = (sdk.models.completions("gpt://b1g6rjppcrrhq56lsqr0/llama-lite/latest@tamrv6si2nqsg5ea5srge"))
-model = model.configure(temperature=0.5, max_tokens=3000)
+model = model.configure(temperature=0.1, max_tokens=3000)
 
 def create_thread():
     return sdk.threads.create(ttl_days=1, expiration_policy="static")
@@ -57,10 +50,6 @@ def create_assistant(model, tools=None):
         model, ttl_days=1, expiration_policy="since_last_active", **kwargs
     )
 
-# Создаем простого ассистента и беседу:
-# thread = create_thread()
-# assistant = create_assistant(model)
-
 
 def get_token_count(filename):
     with open(filename, "r", encoding="utf8") as f:
@@ -72,6 +61,75 @@ def get_file_len(filename):
         return len(f.read())
 
 
+def parse_messages_files(data_dir="messages"):
+    files = []
+
+    for filepath in glob(f"{data_dir}/*.docx", recursive=True):
+        try:
+            #print(f"Обработка файла: {filepath}")
+            doc = Document(filepath)
+
+            content = '\n'.join([para.text for para in doc.paragraphs if para.text.strip()])
+
+            filename = os.path.basename(filepath)
+
+            parts = filename.split('_')
+            user_name = parts[0]
+            id = parts[1]
+            date = parts[2].replace('.docx', '')
+
+            files.append({
+                "content": content,
+                "user_name": user_name,
+                "id": id,
+                "date": date
+            })
+
+        except Exception as e:
+            print(f"\nОшибка при обработке файла {filepath}: {str(e)}")
+            continue
+
+    return files
+
+
+def process_data(data_str):
+
+    # Создаем директорию, если ее нет
+    os.makedirs('processed_data', exist_ok=True)
+
+    with open('processed_data/data.jsonl', 'w', encoding='utf-8') as jsonl_file:
+        data_lines = data_str.strip().split("\n")
+        data_dict = {}
+
+        for line in data_lines:
+            line = line.strip()
+
+            if not line and not data_dict:
+                continue
+
+            if line:
+                if ": " in line:
+                    parts = line.split(": ", 1)
+                    key = parts[0].strip()
+                    value = parts[1].strip() if len(parts) > 1 and parts[1].strip() else ""
+                    data_dict[key] = value
+                elif line.endswith(":"):
+                    data_dict[line[:-1].strip()] = ""
+                else:
+                    data_dict[line.strip()] = ""
+            else:
+                if data_dict:
+                    jsonl_file.write(json.dumps(data_dict, ensure_ascii=False) + '\n')
+                    data_dict = {}
+
+        if data_dict:
+            jsonl_file.write(json.dumps(data_dict, ensure_ascii=False) + '\n')
+
+    #print(f"Обработка завершена. Данные сохранены в processed_data/data.jsonl")
+
+
+
+
 # Основной скрипт
 if __name__ == "__main__":
 
@@ -79,34 +137,37 @@ if __name__ == "__main__":
     assistant = create_assistant(model)
 
     assistant.update(
-        instruction="""Ты — помощник агронома, который должен распределить информацию из собщения по группам: Дата, Подразделение, Операция, Культура, За день га, С начала операции га,Вал за день ц, Вал с начала ц"""
+        instruction="""
+        Ты — помощник агронома, который должен распределить информацию из собщения по группам: 
+        Дата, 
+        Подразделение, 
+        Операция, 
+        Культура, 
+        За день га, 
+        С начала операции га,
+        Вал за день ц, 
+        Вал с начала ц
+        """
     )
 
+    #files = parse_messages_files("messages")
+    #for i in range(len(files)):
+    #    print(files[i]['number_file'])
+    #print(len(files))
     print("\nПробная попытка ассистента...")
     # Пример взаимодействия
     thread = create_thread()
     messages = [message_1, message_2, message_3, message_4, message_5, message_6, message_7]
     for i in range(len(messages)):
-       print(f'Пример {(i + 1)}\n')
-       thread.write(messages[i])
-       response = assistant.run(thread).wait()
-       print(response.text)
+          print(f'Пример {i + 1}\n')
+          thread.write(messages[i])
+          response = assistant.run(thread).wait()
+          print(response.text)
 
-    #thread.write(message_1)
-    #response = assistant.run(thread).wait()
-    #print(response.text)
-
-    # messages = [
-    #     {"role": "system",
-    #      "text": "Ты — помощник агронома, который должен распределить информацию из собщения по группам: Дата, Подразделение, Операция, Культура, За день га, С начала операции га,Вал за день ц, Вал с начала ц"},
-    #     {"role": "user", "text": message_1},
-    #     {"role": "user", "text": "посему ты так думаешь?"}
-    # ]
-    #
-    # print("\nПробная попытка чистой модели...")
-    # result = model.run(messages)
-    #
-    # print(result.text)
+    # thread.write(messages[0])
+    # response = assistant.run(thread).wait()
+    # print(response.text)
+    # process_data(response.text)
 
     thread.delete()
     assistant.delete()
