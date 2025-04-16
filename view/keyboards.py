da95e18c78916f70c9d94258e413aaebd82608ce
azcfza2004 <krokodalmistr@gmail.com>
2025-04-16 11:43:13 +0300

    Delete view/keyboards.py

--- view/keyboards.py
+++
@@ -1,5 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-
-start = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text = 'Старт')]], # Кнопка старт
-                            resize_keyboard= True)
